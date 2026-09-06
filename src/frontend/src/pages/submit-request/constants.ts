1fee2a065c2f5cde273b7d4606aefa6e581be4c9
shoaibugti-spec <shoaibugti@gmail.com>
2026-09-06 16:59:44 +0500

    Add constants for submit request page

---
+++ src/frontend/src/pages/submit-request/constants.ts
@@ -0,0 +1,16 @@
+// src/frontend/src/pages/submit-request/constants.ts
+export const CATEGORY_LIMITS = { /* ... */ };
+export const CATEGORIES = [ /* ... */ ];
+export const STEPS = ["Basic Info", "Category Details", "Verification", "Review"];
+export const CASE_CURRENCIES = [ /* ... */ ];
+export const CURRENCY_SYMBOLS = { /* ... */ };
+export const MAX_FREE_CASES = 2;
+export const MAX_REJECTIONS_BEFORE_SUSPENSION = 5;
+export const UNLOCK_CREDITS_REQUIRED = 5;
+export const DISABILITY_STIPEND_AMOUNT = 6000;
+
+// تمام UTILITY_CATS, LIST_CATS, PAYMENT_RECEIVER_CATS, PERSONAL_PAYMENT_CATS, FIXED_STIPEND_CATS, PROPERTY_RELEVANT_CATS
+// تمام CHOICE_FIELDS, YES_NO_FIELDS, COUNTER_FIELDS, SKIP_FIELDS
+// EDUCATION_SUB_OPTIONS, FEE_SUB_OPTIONS, EDUCATION_ADMISSION_FIELDS, EDUCATION_FEE_FIELDS
+// getEducationDocs, isEasyCat, getCategoryLimit, isFixedAmountCategory, getFixedAmount, getMaxAmount, isDebtPercentageCategory, calculateDebtAmount, urgencyFromDue, getFixedAmountValue, getMaxLimit, isDebtCategory
+// GENDER_OPTIONS, MARITAL_STATUS_OPTIONS
